b19f9d99b34e33a618e6f0e8a3f497cb6faecfb0
gerimarizki <68996917+gerimarizki@users.noreply.github.com>
2021-09-09 19:39:46 +0700

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+console.log("JavaScript berhasil digunakan");
+
+//edited profile
+document.getElementById('name').innerHTML = 'Geri Marizki';
+document.getElementById('date').innerHTML = '31 Maret 200';
+document.getElementById('age').innerHTML = '21 Tahun';
+document.getElementById('city').innerHTML = 'Kota Bandar Lampung';
+document.getElementById('religion').innerHTML = 'Islam';
+document.getElementById('graduate').innerHTML = 'Universitas Teknokrat Indonesia';
